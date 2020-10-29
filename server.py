72eaffbfac058e152898dae9ba332f3f7ba7bf3d
Rene Presedo <presedo93@gmail.com>
2020-10-30 00:46:34 +0100

    🎉 Started basic structure!

---
+++ server.py
@@ -0,0 +1,59 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/mensaje')
+def create_task():
+    return jsonify('Data from Flask')
+
+COUNT = 0
+
+@app.route('/getcount')
+def get_counter():
+    global COUNT
+    return jsonify(COUNT)
+
+@app.route('/setcount', methods=['GET', 'POST'])
+def set_counter():
+    global COUNT
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        COUNT = int(post_data.get('data'))
+        response_object['message'] = 'Counter updated!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(COUNT)
+
+
+@app.route('/addfive', methods=['POST'])
+def addfive_counter():
+    global COUNT
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        COUNT = int(post_data.get('data')) + 5
+        response_object['message'] = 'Counter updated!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(COUNT)
+
+
+@app.route('/subfive', methods=['POST'])
+def subfive_counter():
+    global COUNT
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        COUNT = int(post_data.get('data')) - 5
+        response_object['message'] = 'Counter updated!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(COUNT)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
