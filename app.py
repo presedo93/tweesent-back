64a8358df1192c01e201ddd8a65f1349038bda67
Rene Presedo <presedo93@gmail.com>
2021-04-21 21:04:22 +0200

    🚧 Working on cleaning code

--- app.py
+++ app.py
@@ -4,39 +4,37 @@ from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from routes.predict import Predict, SentimentRequest, SentimentResponse
-from routes.tweet import TweeterBack, TweetRequest, TweetResponse
+from routes.tweet import TweeterBack, TweetRequest, TweetResponse, ErrorResponse
 
 
 with open("config.json") as json_file:
-  conf = json.load(json_file)
+    conf = json.load(json_file)
 
 app = FastAPI()
 torch_nlp = Predict(conf["bert"])
 tweet_api = TweeterBack(conf["tweepy"])
 
 origins = [
-  "http://localhost:8080",
+    "http://localhost:8080",
 ]
 
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=origins,
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-@app.post("/predict", response_model=SentimentResponse)
-async def predict(request: SentimentRequest):
-  sentiment, confidence, probabilities = torch_nlp.predict(request.text)
-  return SentimentResponse(text=request.text, sentiment=sentiment, confidence=confidence, probabilities=probabilities)
 
 @app.post("/gettweet", response_model=TweetResponse)
-async def search(request: TweetRequest, count: int = 200):
-  start = time.time()
-  tweets = tweet_api.search(request.text, count=count)
-  print(f'Fetch tweets: {round(time.time() - start, 3)}s')
-  for tw in tweets:
-    tw['sentiment'], tw['confidence'] = torch_nlp.predict(tweet_api.re_tweet(tw['text']))
-  print(f'Process time: {round(time.time() - start, 3)}s')
-  return TweetResponse(tweets=tweets)
+def search(request: TweetRequest, count: int = 50):
+    # If text starts with @ and is only one word.
+    if len(request.text.split()) == 1 and "@" == list(request.text)[0]:
+        tweets = tweet_api.user(request.text, count=count)
+    else:
+        tweets = tweet_api.search(request.text, count=count)
+
+    for tw in tweets:
+        tw["sentiment"], tw["confidence"] = torch_nlp.predict(tw["text"])
+    return TweetResponse(tweets=tweets)
--- routes/predict.py
+++ routes/predict.py
@@ -11,46 +11,46 @@ from models.sentiment import Sentiment
 
 
 class SentimentRequest(BaseModel):
-  text: str
+    text: str
 
 
 class SentimentResponse(BaseModel):
-  text: str
-  probabilities: Dict[str, float]
-  sentiment: str
-  confidence: float
+    text: str
+    probabilities: Dict[str, float]
+    sentiment: str
+    confidence: float
 
 
 class Predict:
-  def __init__(self, conf) -> None:
-    self.conf = conf
-    self.device = torch.device("cpu")
-    self.tokenizer = BertTokenizer.from_pretrained(conf["type"])
-    classifier = Sentiment(conf)
-    state_dict = torch.load(conf["weights"], map_location=self.device)
-    classifier.load_state_dict(state_dict, strict=False)
-    classifier.eval()
-    self.classifier = classifier.to(self.device)
+    def __init__(self, conf) -> None:
+        self.conf = conf
+        self.device = torch.device("cpu")
+        self.tokenizer = BertTokenizer.from_pretrained(conf["type"])
+        classifier = Sentiment(conf)
+        state_dict = torch.load(conf["weights"], map_location=self.device)
+        classifier.load_state_dict(state_dict, strict=False)
+        classifier.eval()
+        self.classifier = classifier.to(self.device)
 
-  def predict(self, text) -> Tuple[Dict[str, float], str, float]:
-    encoded_text = self.tokenizer.encode_plus(
-        text,
-        truncation=True,
-        max_length=self.conf["maxSeqLen"],
-        add_special_tokens=True,
-        return_token_type_ids=False,
-        padding=True,
-        return_attention_mask=True,
-        return_tensors="pt",
-    )
+    def predict(self, text) -> Tuple[Dict[str, float], str, float]:
+        encoded_text = self.tokenizer.encode_plus(
+            text,
+            truncation=True,
+            max_length=self.conf["maxSeqLen"],
+            add_special_tokens=True,
+            return_token_type_ids=False,
+            padding=True,
+            return_attention_mask=True,
+            return_tensors="pt",
+        )
 
-    encoded_text = {k: v.to(self.device) for k, v in encoded_text.items()}
+        encoded_text = {k: v.to(self.device) for k, v in encoded_text.items()}
 
-    with torch.no_grad():
-      probabilities = f.softmax(self.classifier(encoded_text), dim=1)
-    confidence, predicted_cass = torch.max(probabilities, dim=1)
+        with torch.no_grad():
+            probabilities = f.softmax(self.classifier(encoded_text), dim=1)
+        confidence, predicted_cass = torch.max(probabilities, dim=1)
 
-    return (
-        self.conf["classes"][predicted_cass.cpu().item()],
-        confidence.cpu().item(),
+        return (
+            self.conf["classes"][predicted_cass.cpu().item()],
+            confidence.cpu().item(),
         )
